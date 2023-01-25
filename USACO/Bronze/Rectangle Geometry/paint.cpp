78ee5f0bee26a7b0c5fc496e7307648c17677f79
Camilo Bermúdez <121780547+camilobdez@users.noreply.github.com>
2023-01-25 17:39:54 -0500

    Create paint.cpp

---
+++ USACO/Bronze/Rectangle Geometry/paint.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main() {
+	freopen("paint.in", "r", stdin);
+	freopen("paint.out", "w", stdout);
+	
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	if ((a >= c && a <= d) || (c >= a && c <= b) || (b >= c && b <= d) || (d >= a && d <= b)) {
+		cout << max(b,d) - min(a,c);
+	} else {
+
+	cout << (b-a) + (d-c);
+	}
+
+
+	
+	return 0;
+}
