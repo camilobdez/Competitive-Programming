d7d06206406ec555ef449c3f76e26434d57afaf0
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-01-26 09:18:36 -0500

    Create cowsignal.cpp

---
+++ USACO/Bronze/Simulation/cowsignal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+	freopen("cowsignal.in", "r", stdin);
+	freopen("cowsignal.out", "w", stdout);
+	int M, N, K;
+	cin >> M >> N >> K;
+	string x[M][K*N];
+	for (int i=0; i<M; i++) {
+		for (int j=0; j<N;j++){
+			char c; cin >> c;
+			for (int k=0; k<K; k++) {
+				x[i][K*j+k]=c;
+			}
+		}
+	}
+	for (int i=0; i<M; i++) {
+		for (int j=0;j<K;j++) {
+			for (int p=0;p<K*N;p++) {
+				cout << x[i][p];
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
