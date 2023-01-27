828838a78566286d5ed1092201fdd74121b624c2
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-01-27 07:23:45 -0500

    Add files via upload

---
+++ USACO/Bronze/Simulation/lostcow.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	freopen("lostcow.in", "r", stdin);
+	freopen("lostcow.out", "w", stdout);
+	int x, y;
+	cin >> x >> y;
+	int d=0; int nPos=x; int k=1; int X=x;
+	while (x!=y) {
+		nPos = X + k;
+		if ((nPos>y && x<y) || (x>y && nPos<y)) {
+			d += abs(y-x);
+			break;
+		}
+		d += abs(nPos-x);
+		x = nPos;
+		k *= -2;
+	}
+	cout << d;
+	return 0;
+}
+
