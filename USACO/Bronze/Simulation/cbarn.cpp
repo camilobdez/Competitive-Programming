938c72bfcb24c872bbe0d6fb7b8c06d856fef675
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-02 17:22:00 -0500

    Add files via upload

---
+++ USACO/Bronze/Simulation/cbarn.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	freopen("cbarn.in", "r", stdin);
+	freopen("cbarn.out", "w", stdout);
+	int N; cin >> N; int nums[N];
+	for (int i=0; i<N; i++) {
+		cin >> nums[i];
+	}
+	int mini=INT32_MAX; 
+	for (int i=0; i<N; i++) {
+		int suma=0; int c = 0; int p=0;
+		for (int j=i; j<N; j++) {
+			c++;
+			
+			if (c==N+1) break;
+			if (j==N-1 && c!=N) {
+				//cout << nums[j] << "*" << p<<"\n";
+				suma += nums[j]*p;
+				p++;
+				j=-1;
+			} else {
+				//cout << nums[j] << "*" << p<<"\n";
+				suma += nums[j]*p;
+				p++;
+			}
+		}
+		//cout << "suma: " <<suma << "\n";
+		mini = min(suma,mini);
+	}
+	cout << mini;
+	return 0;
+}
+
+
