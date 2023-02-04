dd52e2f5771bd900e56fc00f3e2087902228051f
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-03 19:11:01 -0500

    Create E.cpp

---
+++ Codeforces/Div. 4/1791/E.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+ 
+	int n, neg=0, cero=0, minn=1e9;
+	cin >> n;
+	long long sa=0;
+ 
+	for (int i=0; i<n; i++) {
+		int a; cin >> a;
+		sa += abs(a);
+		minn = min(minn, abs(a));
+		neg += a<0;
+		cero += a==0;
+	}
+ 
+	if (cero || neg%2==0) {
+		cout << sa << endl;
+	} else {
+		cout << sa - 2*minn << endl;
+	}
+ 
+}
+ 
+int main() {
+ 
+	int t; cin >> t;
+	while (t--) {
+		solve();
+	}
+ 
+	return 0;
+}
