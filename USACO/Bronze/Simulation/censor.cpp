d79137dc1dda7c0ff331aff58faa132574121ec4
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-07 22:12:59 -0500

    Update censor.cpp

--- USACO/Bronze/Simulation/censor.cpp
+++ USACO/Bronze/Simulation/censor.cpp
@@ -1,29 +1,22 @@
+//link: http://www.usaco.org/index.php?page=viewproblem2&cpid=526
 #include <bits/stdc++.h>
-
-
 using namespace std;
-
 
 int main() {
 	freopen("censor.in", "r", stdin);
 	freopen("censor.out", "w", stdout);
 	
-	string s;
-	string t;
+	string s, t;
+	
 	cin >> s >> t;
 	string c="";
 	int n = s.size();
 	int i=0;
 	while (i < s.size()) {
 		c += s[i];
-		
-		if (c.size() > t.size() && c.substr(c.size()-t.size(), t.size())==t) {
-			c.erase(c.size()-t.size(), t.size());
-		}
+		if (c.size() > t.size() && c.substr(c.size()-t.size(), t.size())==t) c.erase(c.size()-t.size(), t.size());
 		i++;
 	}
 	cout << c << endl;
-	
-	
 	return 0;
 }
