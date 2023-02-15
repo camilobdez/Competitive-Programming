33d0af629b71deb0643e3931f2191f3eac4d9af5
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-14 20:40:45 -0500

    Create maxdistance.cpp

---
+++ USACO/Bronze/Basic Complete Search/maxdistance.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	
+	int n, maximo=0;
+	cin >> n;
+ 
+	int xc[n], yc[n];
+ 
+	for (int i=0; i < n; i++) {
+		cin >> xc[i];
+	}
+ 
+	for (int i=0; i < n; i++) {
+		cin >> yc[i];
+	}
+ 
+	for (int i=0; i < n-1; i++) {
+		for (int j=i+1; j < n; j++) {
+			int distance = pow(xc[i]-xc[j], 2) + pow (yc[i]-yc[j], 2);
+			maximo = max(distance, maximo);
+		}	
+	}
+  
+	cout << maximo << endl;
+	return 0;
+}
