1068bc2397db814e55a824c0e2adf9dcf61f26d6
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-15 11:54:11 -0500

    Update cownomics.cpp

--- USACO/Bronze/Basic Complete Search/cownomics.cpp
+++ USACO/Bronze/Basic Complete Search/cownomics.cpp
@@ -1,47 +1,43 @@
+//link: http://www.usaco.org/index.php?page=viewproblem2&cpid=736
 #include <bits/stdc++.h>
 using namespace std;
-
 
 int main() {
 	freopen("cownomics.in", "r", stdin);
 	freopen("cownomics.out", "w", stdout);
 	
-	int N, M; 
+	int n, m; 
 	
-	cin >> N >> M;
+	cin >> n >> n;
 
-	char spot[N][M];
-	char plain[N][M];
+	char spot[n][m], plain[n][m];
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<M; j++) {
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<m; j++) {
 			cin >> spot[i][j];
 		}
 	}
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<M; j++) {
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<m; j++) {
 			cin >> plain[i][j];
 		}
 	}
 
 	int out=0;
 	
-	for (int i=0; i<M; i++) {
+	for (int i=0; i<m; i++) {
 		int row=0;
 		bool l2=false;
 		//cout << spot[row][i] << endl << plain[row][i] << endl;
 label2:
-		if (spot[row][i]==plain[row][i]) {
-			continue;
-		}
-
+		if (spot[row][i]==plain[row][i]) continue;
+		
 		for (int j=0; j<N; j++) {
 			//cout << spot[row][i] << endl << plain[row][i] << endl;
-			if (spot[row][i]==plain[j][i]) {
-				goto label;
-			}
+			if (spot[row][i]==plain[j][i]) goto label;
 		}
+		
 		l2=true;
 		if (row+1 <N) {
 			row++;
