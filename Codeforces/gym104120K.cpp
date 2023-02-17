3435f428fa28e417531c52e13b79a83cbf14518c
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-17 16:09:37 -0500

    Update gym104120K.cpp

--- Codeforces/gym104120K.cpp
+++ Codeforces/gym104120K.cpp
@@ -6,17 +6,17 @@ int main(){
 	string lm[8] = 
 	{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 	
-	string codes[n];
+	
 	map<string,int> m;
  
 	for (int i=0; i<n; i++) {
-		string s; cin >> s;
+		string s, c=""; cin >> s;
 		for (int j=0; j<s.size(); j++) {
 			for (int k=0; k<8; k++) {
 				bool f=false;
 				for (int u=0; u<lm[k].size(); u++) {
 					if (s[j]==lm[k][u]) {
-						codes[i]+=to_string(k+2);
+						c+=to_string(k+2);
 						f=true;
 						break;
 					}
@@ -24,7 +24,7 @@ int main(){
 				if (f) break;
 			}
 		}
-		m[codes[i]]++;
+		m[c]++;
 		//cout << codes[i] << endl;
 	}
  
