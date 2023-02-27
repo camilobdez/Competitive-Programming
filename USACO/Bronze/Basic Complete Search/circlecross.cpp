1b428e7272ffe319a55772db5c900ebd75417a16
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-02-27 18:17:52 -0500

    Add files via upload

---
+++ USACO/Bronze/Basic Complete Search/circlecross.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	//freopen("circlecross.in", "r", stdin);
+	//freopen("circlecross.out", "w", stdout);
+	
+	string S;
+	
+	cin >> S;
+
+	int out=0;
+
+	for (int a = 0; a < S.size(); a++) {
+		for (int b =a+1; b < S.size(); b++) {
+			for (int c=b+1; c < S.size(); c++) {
+				for (int d=c+1; d < S.size(); d++) {
+					if (S[a]==S[c] && S[b]==S[d]) {
+						out++;
+					}
+				}
+			}
+		}
+	}
+	cout << out <<endl;
+
+	return 0;
+}
