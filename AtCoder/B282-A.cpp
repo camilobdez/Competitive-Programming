c5d22816b30fb06318531d4b8d922c8ac57a8c83
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-04-17 16:00:07 -0500

    Update B282-A.cpp

--- AtCoder/B282-A.cpp
+++ AtCoder/B282-A.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+
+using namespace std;
+
+
+int main() {
+	int K;
+	cin >> K;
+
+	string a[26]={"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+
+	for (int i=0; i<K;i++){
+		cout << a[i];
+	}
+	
+	
+	return 0;
+}
