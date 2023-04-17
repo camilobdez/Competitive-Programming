9237314d09e5490ffc385b599e4152d4044e2637
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-04-17 15:58:22 -0500

    Create B297-D.cpp

---
+++ AtCoder/B297-D.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	long long a, b, c=0;
+	cin >> a >> b;
+	while (a!=b) {
+	    if (a%b==0 || b%a==0) {
+	        c += max(a/b, b/a);
+	        break;
+	    } else {
+	        if (max(a,b)/min(a,b)>=2) {
+	            long long g = max(a, b), p = min(a, b);
+	            g -= p*g/p, a = g, b = p, c+=d;
+	        } else {
+	            if (a > b) {
+	                a -= b;
+	            } else {
+	                b -= a;
+	            }
+	            c++;
+	        }
+	    }
+	}
+	if (c-1>=0) {
+	    c=c-1;
+	} else {
+	    c=0;
+	}
+	cout << c;
+
+	return 0;
+}
