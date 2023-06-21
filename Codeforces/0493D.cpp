6787d0a6ae19988afa52950d7667b1fe1fcc5a9f
camilobdez <121780547+camilobdez@users.noreply.github.com>
2023-06-21 04:49:39 -0500

    Create 0493D.cpp

---
+++ Codeforces/0493D.cpp
@@ -0,0 +1,13 @@
+# include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int n;
+	cin >> n;
+	if(n % 2 == 0) {
+		cout<< "white" << endl;
+		cout<< 1 << " " << 2 << endl;
+	} else {
+		cout << "black" << endl;
+	}
+}
